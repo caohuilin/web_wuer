7179b390755ce67f66627a2153a5084c5214f107
caohuilin <belinda@berkelium.local>
2016-06-12 11:50:23 +0800

    prize 界面实现

--- src/jsx/Section3.js
+++ src/jsx/Section3.js
@@ -1,7 +1,21 @@
 const Section3 = React.createClass({
     render(){
+        const list = Prize.map((p,i)=>{
+           return(
+               <li>
+                   <img src={p.src}/>
+                   <div className="des">{p.name}</div>
+               </li>
+           )
+        });
         return (
-            <div>Section3</div>
+            <section className="section3">
+                <div className="logo"><img src="../../public/img/logo_prize.png" alt=""/></div>
+                <ul className="prize">
+                    {list}
+                </ul>
+
+            </section>
         )
     }
 });
