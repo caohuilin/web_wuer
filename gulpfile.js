5e766bf8958458fb7daa1f5b78d6435a408ac4df
caohuilin <belinda@berkelium.local>
2016-06-14 23:39:54 +0800

    编译ES6

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var path = require('path');
 var react = require('gulp-react');
 var concat = require('gulp-concat');
 var clean = require('gulp-clean');
+var babel = require('gulp-babel');
 var runSequence = require('run-sequence');
 
 gulp.task('default', function () {
@@ -22,7 +23,10 @@ gulp.task('less', function () {
 
 gulp.task('jsx', function () {
     return gulp.src(["./src/jsx/Util.js","./src/jsx/Config.js","./src/jsx/Story.js","./src/jsx/Prize.js","./src/jsx/User.js","./src/jsx/Login.js","./src/jsx/Section5.js","./src/jsx/Section4.js","./src/jsx/Section3.js","./src/jsx/Section2.js","./src/jsx/Section1.js","./src/jsx/Content.js","./src/jsx/Nav.js","./src/jsx/Header.js","./src/jsx/Main.js","./src/jsx/Router.js"])
-        .pipe(react())
+        .pipe(babel({
+            presets: ["react",'es2015']
+
+        }))
         .on('error', function(err){
             console.log(err);
             this.end();
